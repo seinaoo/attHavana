0d5365316c8385134294e300165ac8b6909038ca
seinaoo <161845569+seinaoo@users.noreply.github.com>
2024-11-25 20:04:55 -0300

    Update Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Cachorro extends Animal {
+    public Cachorro(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Cachorro " + getNome() + ": Au au!");
+    }
+}
