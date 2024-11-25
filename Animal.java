b9f89b584491d1687dc52d55937b30f0cf7a4da3
seinaoo <161845569+seinaoo@users.noreply.github.com>
2024-11-25 20:04:21 -0300

    Update Animal.java

---
+++ Animal.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public abstract class Animal {
+    private String nome;
+    private int idade;
+
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public abstract void emitirSom();
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
